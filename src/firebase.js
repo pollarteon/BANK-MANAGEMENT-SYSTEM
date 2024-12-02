931aa77a70715e9f9e898924327b6ed8f5069c08
niruk2004 <niranjank2929@gmail.com>
2024-12-03 05:14:02 +0530

    removed unneccessary import from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth,signInWithCredential,EmailAuthProvider } from "firebase/auth";
+import { getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
